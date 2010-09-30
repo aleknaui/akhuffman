15dcf3eb1906ab2aa065aa22d2d7f670a54e09de
ale.knaui <ale.knaui@79b6f98e-869c-25fb-a88a-7da6f697a2c8>
2010-09-30 17:26:12 +0000

    Se agregó el Nodo para arbol binario y su test.

---
+++ trunk/source/tests/TestNodo.java
@@ -0,0 +1,38 @@
+package tests;
+
+import estructuras.Nodo;
+import junit.framework.TestCase;
+
+public class TestNodo extends TestCase {
+
+	protected Nodo<String> nodo;
+	
+	public TestNodo(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		nodo = new Nodo<String>("casa");
+	}
+	
+	public final void testDarValor(){
+		assertEquals("casa",nodo.darValor());
+	}
+	
+	public final void testSetHijo(){
+		nodo.setHijo("carro");
+		assertEquals("carro",nodo.darHijoIzquierdo().darValor());
+		nodo.setHijo( "cosa" );
+		assertEquals("cosa",nodo.darHijoDerecho().darValor());
+		assertEquals(false, nodo.setHijo("carro"));
+	}
+	
+	public final void testIsEmpty(){
+		assertEquals(false,nodo.isEmpty());
+	}
+	
+	protected void tearDown() throws Exception {
+		nodo = null;
+	}
+
+}
