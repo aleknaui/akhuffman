6a4e0d0775de9ab3776820fe48123eea04bf1fc2
ale.knaui <ale.knaui@79b6f98e-869c-25fb-a88a-7da6f697a2c8>
2010-09-30 20:01:41 +0000

    Implementación de ArbolBinario. El siguiente paso es realizar las pruebas.

--- trunk/ArbolBinario/src/Tests/TestNodo.java
+++ trunk/ArbolBinario/src/Tests/TestNodo.java
@@ -1,35 +1,34 @@
-package tests;
+package Tests;
 
-import estructuras.Nodo;
+import Code.Nodo;
 import junit.framework.TestCase;
 
 public class TestNodo extends TestCase {
 
-	/** Nodo en el que se realizarán las pruebas */
 	protected Nodo<String> nodo;
 	
-	/** Crea el test */
 	public TestNodo(String name) {
 		super(name);
 	}
 
-	/** Prepara la clase para realizar pruebas. Instancia el nodo y como objeto le asigna "casa". */
 	protected void setUp() throws Exception {
 		nodo = new Nodo<String>("casa");
 	}
 	
-	/** Prueba de recuperación del valor del nodo. */
 	public final void testDarValor(){
 		assertEquals("casa",nodo.darValor());
 	}
 	
-	/**  */
 	public final void testSetHijo(){
 		nodo.setHijo("carro");
 		assertEquals("carro",nodo.darHijoIzquierdo().darValor());
 		nodo.setHijo( "cosa" );
 		assertEquals("cosa",nodo.darHijoDerecho().darValor());
 		assertEquals(false, nodo.setHijo("carro"));
+	}
+	
+	public final void testIsEmpty(){
+		assertEquals(false,nodo.isEmpty());
 	}
 	
 	protected void tearDown() throws Exception {
